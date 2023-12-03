eab07f62cdb6e79aa6bf43999c2de5b487f33a0a
kun.liu <kun.liu@outlook.com>
2023-12-03 11:44:22 +0800

    updated the configuration for postgresql database in config/database.cr. updated the listen port to 0.0.0.0 from 127.0.0.1 in config/watch.yml.

--- config/database.cr
+++ config/database.cr
@@ -7,11 +7,11 @@ AppDatabase.configure do |settings|
     settings.credentials = Avram::Credentials.parse?(ENV["DATABASE_URL"]?) || Avram::Credentials.new(
       database: database_name,
       hostname: ENV["DB_HOST"]? || "localhost",
-      port: ENV["DB_PORT"]?.try(&.to_i) || 5432,
+      port: ENV["DB_PORT"]?.try(&.to_i) || 6336,
       # Some common usernames are "postgres", "root", or your system username (run 'whoami')
       username: ENV["DB_USERNAME"]? || "postgres",
       # Some Postgres installations require no password. Use "" if that is the case.
-      password: ENV["DB_PASSWORD"]? || "postgres"
+      password: ENV["DB_PASSWORD"]? || "WqA1yT2z+!"
     )
   end
 end

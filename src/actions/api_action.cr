69284238169e0663a58d60f64029dc0cf770a4e2
kun.liu <kun.liu@outlook.com>
2023-12-03 11:51:31 +0800

    added the route prefix to class ApiAction in src/actions/api_action.cr.

--- src/actions/api_action.cr
+++ src/actions/api_action.cr
@@ -3,7 +3,7 @@ abstract class ApiAction < Lucky::Action
   # APIs typically do not need to send cookie/session data.
   # Remove this line if you want to send cookies in the response header.
   disable_cookies
-  accepted_formats [:json]
+  accepted_formats [:json], default: :json
 
   include Api::Auth::Helpers
 
@@ -14,4 +14,6 @@ abstract class ApiAction < Lucky::Action
   # By default all actions are required to use underscores to separate words.
   # Add 'include Lucky::SkipRouteStyleCheck' to your actions if you wish to ignore this check for specific routes.
   include Lucky::EnforceUnderscoredRoute
+
+  route_prefix "/api/v1"
 end

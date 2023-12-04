22aade522fdca773a1077dc775c19ea9783c0de5
kun.liu <kun.liu@outlook.com>
2023-12-04 17:24:20 +0800

    added comments about response with 201 instead of 200 with token.

--- src/actions/api/sign_ups/create.cr
+++ src/actions/api/sign_ups/create.cr
@@ -3,6 +3,9 @@ class Api::SignUps::Create < ApiAction
 
   post "/api/sign_ups" do
     user = SignUpUser.create!(params)
+    # response with 200 OK with token
     json({token: UserToken.generate(user)})
+    # or just response with 201 Created
+    # head HTTP::Status::Created
   end
 end

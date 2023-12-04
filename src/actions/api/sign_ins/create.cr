9663ff8533ab907d611cb00bed1025b894845ee2
kun.liu <kun.liu@outlook.com>
2023-12-04 20:11:23 +0800

    added request data validation in sign_ins api.

--- src/actions/api/sign_ins/create.cr
+++ src/actions/api/sign_ins/create.cr
@@ -1,12 +1,31 @@
+class SignInRequestUser
+  include JSON::Serializable
+  property email : String
+  property password : String
+end
+
+class SignInRequest
+  include JSON::Serializable
+  property user : SignInRequestUser
+end
+
 class Api::SignIns::Create < ApiAction
   include Api::Auth::SkipRequireAuthToken
 
-  post "/api/sign_ins" do
-    SignInUser.run(params) do |operation, user|
-      if user
-        json({token: UserToken.generate(user)})
-      else
-        raise Avram::InvalidOperationError.new(operation)
+  post "/sign_ins" do
+    begin
+      req = SignInRequest.from_json(params.body)
+    rescue JSON::SerializableError
+      json({
+        message: "The request data is unexpected",
+      }, HTTP::Status::BAD_REQUEST)
+    else
+      SignInUser.run(params) do |operation, user|
+        if user
+          json({token: UserToken.generate(user)})
+        else
+          raise Avram::InvalidOperationError.new(operation)
+        end
       end
     end
   end

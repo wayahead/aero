b3718058fab4fb1532421099528174ebcbeb327e
kun.liu <kun.liu@outlook.com>
2023-12-04 20:11:44 +0800

    added request data validation in sign_ups api.

--- src/actions/api/sign_ups/create.cr
+++ src/actions/api/sign_ups/create.cr
@@ -1,11 +1,35 @@
+class SignUpRequestUser
+  include JSON::Serializable
+  property email : String
+  property password : String
+  property password_confirmation : String
+end
+
+class SignUpRequest
+  include JSON::Serializable
+  property user : SignUpRequestUser
+end
+
 class Api::SignUps::Create < ApiAction
   include Api::Auth::SkipRequireAuthToken
 
-  post "/api/sign_ups" do
-    user = SignUpUser.create!(params)
-    # response with 200 OK with token
-    json({token: UserToken.generate(user)})
-    # or just response with 201 Created
-    # head HTTP::Status::Created
+  post "/sign_ups" do
+    begin
+      req = SignUpRequest.from_json(params.body)
+      user = SignUpUser.create!(params)
+    rescue JSON::SerializableError
+      json({
+        message: "The request data is unexpected",
+      }, HTTP::Status::BAD_REQUEST)
+    rescue TypeCastError
+      json({
+        message: "Unexpected request data",
+      }, HTTP::Status::BAD_REQUEST)
+    else
+      # response with 200 OK with token
+      json({token: UserToken.generate(user)})
+      # or just response with 201 Created
+      # head HTTP::Status::Created
+    end
   end
 end
